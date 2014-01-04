10de97fb86967e3476725d8c055bb023acd0db91
Roll <Roll>
2014-01-04 23:56:13 +0200

    newsready

--- client/pictor.js
+++ client/pictor.js
@@ -27,11 +27,27 @@ Template.privateMessagePanel.messages= function () {
 Template.privateMessagePanel.user= function () {
   return Meteor.users.findOne({_id : this._id});
 }; 
- 
+
 Template.myMessages.messages= function () {
   return Messages.find({to_id: Meteor.userId()}, {sort: {created_on:-1}});  
 };
- 
+
+Handlebars.registerHelper('fullName', function(person) {
+  var a = Meteor.users.findOne({_id : person});
+  return a.profile.fullFirstName + " " + a.profile.fullLastName;
+});
+
+Handlebars.registerHelper('checkName', function() {
+  if (Meteor.user().profile && (Meteor.user().profile.fullFirstName || Meteor.user().profile.fullLastName)) return true;
+  return false;
+});
+
+/*Template.myMessages.users= function () {
+  //var user = Meteor.users.findOne({_id : from_id}); 
+  //return user.profile.fullFirstName + " "+user.profile.fullLastName;
+  return Meteor.users.find();
+};*/
+
 Template.textarea.user= function () {
   return Meteor.user();
 };
@@ -42,18 +58,23 @@ Template.jobs.jobs= function () {
 
 Template.form.events({
   'click button#buttonNew' : function () {
-    if (!$('#textarea').val()) {}
+    if (!$('#textarea').val()) {
+      $('#alert').css('color','red');
+    }
     else { 
       var options = { ownPost: $("#textarea").val() }; 
       if (Meteor.user()) {
-        if (Meteor.user().profile && Meteor.user().profile.fullFirstName) {
+        if (Meteor.user().profile && (Meteor.user().profile.fullFirstName || Meteor.user().profile.fullLastName)) {
           options.name = Meteor.user().profile.fullFirstName;  
-          //options.lastname = Meteor.user().profile.fullLastName;
+          options.lastname = Meteor.user().profile.fullLastName;
+
           Posts.insert(options);
           $('#textarea').val('');
+          $('#alert').css('color','#777777');
         }
         else {
-          if (!$('#fullFirstNameSimplyRegistration').val()) {
+          if (!$('#fullFirstNameSimplyRegistration').val() && !$('#fullLastNameSimplyRegistration').val()) {
+            $('.enterfirstname').text("Введите имя"); 
           }
           else {
             options.name = $('#fullFirstNameSimplyRegistration').val();  
@@ -72,9 +93,16 @@ Template.form.events({
         }
       }
       else {
-        options.name = $('#firstName').val();
-        Posts.insert(options);
-        $('#textarea').val('');
+        if (!$('#firstName').val()) {
+          $('.enterfirstname').text("Введите имя"); 
+        }
+        else {
+          options.name = $('#firstName').val();
+          Posts.insert(options);
+          $('#textarea').val('');
+          $('#alert').css('color','#777777');
+          $('.enterfirstname').text(""); 
+        }
       }
     }            
   }
@@ -139,16 +167,32 @@ Template.privateMessagePanel.events({
                        };
       if (Meteor.user()) {
         options.from_id = Meteor.userId();
-        options.username = Meteor.user().profile.fullFirstName;
-        options.lastname = Meteor.user().profile.fullLastName;
+        if(!Meteor.user().profile.fullFirstName){
+          if (!$('#fullFirstNameSimplyRegistration').val() || !$('#fullLastNameSimplyRegistration').val()) {}
+          else {
+            Meteor.users.update(Meteor.userId(), {
+              $set: {
+                profile: {
+                  fullFirstName : $('#fullFirstNameSimplyRegistration').val(), 
+                  fullLastName : $('#fullLastNameSimplyRegistration').val(), 
+                }
               }
+            });
+            Messages.insert(options);
+            $('#textarea').val(''); 
+          } 
+        }
+        else {
+          Messages.insert(options);
+          $('#textarea').val(''); 
+        }
+      }
       else {
         options.username = $('#firstName').val();
+        Messages.insert(options);
+        $('#textarea').val(''); 
       }
-      Messages.insert(options);
-    }
- 
-    $('#textarea').val('');           
+    }          
   }
 });
 
